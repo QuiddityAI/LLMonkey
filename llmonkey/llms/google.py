bfe3b1ce81e2cf4817c9b8a3db60dce19372f5a9
Tim Henning <tim@luminosuslight.de>
2024-11-03 19:02:25 +0100

    Update Google models

--- llmonkey/llms/google.py
+++ llmonkey/llms/google.py
@@ -2,6 +2,21 @@ from ..models import ModelCapabilities, ModelConfig, ModelLocation, ModelProvide
 from .base_llm import BaseLLMModel
 
 
+class Google_Gemini_Flash_1_5_v1(BaseLLMModel):
+    config = ModelConfig(
+        identifier="gemini-1.5-flash-001",
+        verbose_name="Google Gemini Flash 1.5 v1",
+        description="Google's Gemini Flash 1.5 model, version 1",
+        max_input_tokens=1_048_576,
+        euro_per_1M_input_tokens=0.017,
+        euro_per_1M_output_tokens=0.07,
+        capabilities=[ModelCapabilities.chat, ModelCapabilities.vision],
+        location=ModelLocation.EU,
+        parameters="~16B",
+    )
+    provider = ModelProvider.google
+
+
 class Google_Gemini_Flash_1_5_v2(BaseLLMModel):
     config = ModelConfig(
         identifier="gemini-1.5-flash-002",
@@ -19,7 +34,7 @@ class Google_Gemini_Flash_1_5_v2(BaseLLMModel):
 
 class Google_Gemini_Flash_1_5_8B(BaseLLMModel):
     config = ModelConfig(
-        identifier="gemini-1.5-flash-8b-latest",
+        identifier="gemini-1.5-flash-8b",
         verbose_name="Google Gemini Flash 1.5 8B",
         description="Google's Gemini Flash 1.5 model, 8B version",
         max_input_tokens=1_048_576,
